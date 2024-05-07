04bd1998d21f93dc8dccd77d1224240885d3cd24
DarshanBattula <114302987+darshan1005@users.noreply.github.com>
2024-05-08 01:38:09 +0530

    Add files via upload

---
+++ artist-data.js
@@ -0,0 +1,35 @@
+const artistContainer = document.querySelector('.artist-list');
+
+let artistData = [
+  {
+    id: 1,
+    img: 'artist/singer2.jpg',
+    name: 'Singer 1'
+  },
+  {
+    id: 2,
+    img: 'artist/singer3.jpg',
+    name: 'Singer 2'
+  },
+  {
+    id: 3,
+    img: 'artist/SPB.jpg',
+    name: 'SPB'
+  },
+  {
+    id: 4,
+    img: 'artist/SPB.jpg',
+    name: 'SPB'
+  }
+];
+
+artistData.forEach(artist => {
+  const listItem = document.createElement('li');
+  
+  const image = document.createElement('img');
+  image.src = artist.img;
+  image.alt = artist.name;
+  
+  listItem.appendChild(image);
+  artistContainer.appendChild(listItem);
+});
