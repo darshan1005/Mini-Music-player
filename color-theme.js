4de8aba9e6c62f5baba226db9694d0c604b2b905
DarshanBattula <114302987+darshan1005@users.noreply.github.com>
2024-05-08 23:44:29 +0530

    Update color-theme.js

--- color-theme.js
+++ color-theme.js
@@ -1,23 +1,23 @@
-const beautifulColors = [
-    '#FFD700', // Gold
-    '#FFA07A', // Light Salmon
-    '#FFC0CB', // Pink
-    '#F08080', // Light Coral
-    '#87CEEB', // Sky Blue
-    '#20B2AA', // Light Sea Green
-    '#9370DB', // Medium Purple
-    '#FA8072', // Salmon
-    '#FFDAB9', // Peachpuff
-    '#00CED1', // Dark Turquoise
-  ];
+const Shades = [
+  '#111111', // Very Dark Gray
+  '#1A1A1A', // Dark Gray
+  '#222222', // Dim Gray
+  '#333333', // Charcoal
+  '#444444', // Gray
+  '#444444', // Gray
+  '#333333', // Charcoal
+  '#222222', // Dim Gray
+  '#1A1A1A', // Dark Gray
+  '#111111', // Very Dark Gray
+];
   
 let colorIndex = 0; 
 
 function changeBackgroundColor() {
     const musicHead = document.querySelector('.container .music-head');
     musicHead.style.transition = 'background-color 0.8s ease'; 
-    musicHead.style.background = beautifulColors[colorIndex];
-    colorIndex = (colorIndex + 1) % beautifulColors.length; 
+    musicHead.style.background = Shades[colorIndex];
+    colorIndex = (colorIndex + 1) % Shades.length; 
 }
 
 changeBackgroundColor();
