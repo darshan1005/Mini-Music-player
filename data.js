70ed235b7b88afe6c97860f860bdd6b6a76aef93
DarshanBattula <114302987+darshan1005@users.noreply.github.com>
2024-05-20 17:24:31 +0530

    Update data.js

--- data.js
+++ data.js
@@ -4,97 +4,111 @@ let allmusic = [
         name: 'By our love',
         artist: 'Peter Scholtes',
         img: 'images/jesus',
-        src: 'music/By our love'
+        src: 'music/By Our Love',
+        favSong: false
     },
     {
         index: 2,
         name: 'Let her go',
         artist: 'passenger',
         img: 'images/let-her-go',
-        src: 'music/Let Her Go'
+        src: 'music/Let Her Go',
+        favSong: false
     },
     {
         index: 3,
         name: 'Gaaju Bomma',
         artist: 'Hesham',
         img: 'images/hi-nanna',
-        src: 'music/Gaaju bomma'
+        src: 'music/Gaaju Bomma',
+        favSong: false
     },
     {
         index: 4,
         name: 'Odiyamma',
         artist: 'Dhruv Vikram',
         img: 'images/hi-nanna',
-        src: 'music/Odiyamma'
+        src: 'music/Odiyamma',
+        favSong: false
     }, 
     {
         index: 5,
         name: 'Enno Enno',
         artist: 'Bhavana Isvi',
         img: 'images/hi-nanna',
-        src: 'music/Enno Enno'
+        src: 'music/Enno Enno',
+        favSong: false
     }, 
     {
         index: 6,
         name: 'Despacito',
         artist: 'Luis Fonsi',
         img: 'images/despacito',
-        src: 'music/Despacito'
+        src: 'music/Despacito',
+        favSong: false
     },
     {
         index: 7,
         name: 'People',
         artist: 'Libinca',
         img: 'images/people',
-        src: 'music/People'
+        src: 'music/People',
+        favSong: false
     },
     {
         index: 8,
         name: 'High school',
         artist: 'Nicki Minaj',
         img: 'images/High-school',
-        src: 'music/High School'
+        src: 'music/High School',
+        favSong: false
     },
     {
         index: 9,
         name: 'I just wanna be your',
         artist: 'ArcticMonkeys',
         img: 'images/wann-be-yours',
-        src: 'music/I-Wanna-Be-Yours'
+        src: 'music/I-Wanna-Be-Yours',
+        favSong: false
     },
     {
         index: 10,
         name: 'Shivers',
         artist: 'Edsheern',
         img: 'images/Shivers-Art',
-        src: 'music/shivers'
+        src: 'music/shivers',
+        favSong: false
     },
     {
         index: 11,
         name: 'Bad-Habits',
         artist: 'Edsheern',
         img: 'images/bad-habits',
-        src: 'music/Bad Habits'
+        src: 'music/Bad Habits',
+        favSong: false
     }, 
     {
         index: 12,
         name: 'Shape of you',
         artist: 'Edsheern',
         img: 'images/shape-of-you',
-        src: 'music/shape-of-you'
+        src: 'music/shape-of-you',
+        favSong: false
     },
     {
         index: 13,
-        name: 'Shape of u-Indian ragas',
+        name: 'Indian ragas',
         artist: 'Indian Ragas',
         img: 'images/indian-ragas',
-        src: 'music/shape-of-you-indian-ragas'
+        src: 'music/shape-of-you-indian-ragas',
+        favSong: false
     },
     {
         index: 14,
         name: 'Love Story',
         artist: 'Indila',
         img: 'images/love-story',
-        src: 'music/love-story'
+        src: 'music/love-story',
+        favSong: false
     },
 ];
